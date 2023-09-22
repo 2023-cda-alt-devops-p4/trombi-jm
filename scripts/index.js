9d7193654246fbb4a774cbcbdeb14617d4474fda
Jonathan Moreschi <j.moreschi@outlook.fr>
2023-09-22 16:07:11 +0200

    feat: add profil pictures, finnish JSON file

--- scripts/index.js
+++ scripts/index.js
@@ -9,13 +9,16 @@ const context = canvas.getContext('2d')
 // constants
 const pinWidth = 30
 const pinHeight = 50
+
+// display Map
 mapBackground(context)
 
 // Fetch the JSON file with the infos
-fetch('/scripts/test.json')
+fetch('/scripts/info.json')
   .then(res => res.json())
   .then(infos => {
-    console.log(infos)
+    const showinfos = document.querySelector('#show_infos')
+    showinfos.style.visibility = 'hidden'
 
     // functions calls
     infos.forEach((item) => {
@@ -26,6 +29,12 @@ fetch('/scripts/test.json')
 
     canvas.addEventListener('click', (e) => {
       const name = document.querySelector('#name')
+      const city = document.querySelector('#city')
+      const hobbies = document.querySelector('#hobbies')
+      const stack = document.querySelector('#stack')
+      const linkedin = document.querySelector('#linkedin')
+      const picture = document.querySelector('#picture')
+      showinfos.style.visibility = 'visible'
 
       const x = e.offsetX
       const y = e.offsetY
@@ -34,6 +43,24 @@ fetch('/scripts/test.json')
         const yPinPos = item.map.y - pinWidth
         if (x >= xPinPos && x <= xPinPos + pinWidth && y >= yPinPos && y <= yPinPos + pinHeight) {
           name.textContent = item.name
+          stack.textContent = item.stack
+          linkedin.textContent = item.linkedin
+          linkedin.href = item.linkedin
+          hobbies.textContent = item.hobbies
+          city.textContent = item.city
+          picture.innerHTML = `<img src="${item.photo}" alt="${item.name}" />`
+        }
+      })
+    })
+
+    canvas.addEventListener('mouseover', (e) => {
+      const x = e.offsetX
+      const y = e.offsetY
+      infos.forEach(item => {
+        const xPinPos = item.map.x - pinHeight / 2
+        const yPinPos = item.map.y - pinWidth
+        if (x >= xPinPos && x <= xPinPos + pinWidth && y >= yPinPos && y <= yPinPos + pinHeight) {
+          canvas.style.cursor = 'pointer'
         }
       })
     })
